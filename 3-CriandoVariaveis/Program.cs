3dbdc2584598d44fa475c9fbaab0c9e5843774f5
Thiago Alves <101125484+ThiagoAlv1@users.noreply.github.com>
2023-04-06 22:03:24 -0300

    first commit

---
+++ 3-CriandoVariaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 3 - Criando Variáveis");
+
+        int idade;
+        idade = 25;
+        Console.WriteLine("Tenho " + idade + " anos de idade");
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        //O double é um tipo de variável para números inteiros e reais
+        double salario = 3000.15;
+            Console.WriteLine(salario); 
+
+        //O short é um tipo de variável de 32 bits
+        short x = 1500;
+            Console.WriteLine(x);
+        
+        //O int é um tipo de variável para números inteiros
+        //Aqui, convertemos a variavei salario para um número inteiro através do casting
+        int salarioInteiro = (int)salario;
+            Console.WriteLine(salarioInteiro);
+       
+        //O long é um tipo de variável de 64 bits
+        long y = 200000000000000;
+            Console.WriteLine(y);
+
+        float altura = 1.62f;
+            Console.WriteLine(altura);
+        
+
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+
+    }
+}
