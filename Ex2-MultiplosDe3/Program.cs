64ce6188655c8985c09d1b6226eb397d30fa6b5c
Thiago Alves <101125484+ThiagoAlv1@users.noreply.github.com>
2023-04-09 23:49:06 -0300

    commit

---
+++ Ex2-MultiplosDe3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ex 2 - Multiplos de 3");
+
+     
+
+        for (int multiplos = 0; multiplos < 100; multiplos++)
+        {
+            if ((multiplos % 3) == 0)
+            {
+                Console.WriteLine(multiplos);
+            }
+        }
+       
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+
+    }
+}
